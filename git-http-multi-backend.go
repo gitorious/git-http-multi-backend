d01b557e1b5b136437438598019896904299c556
Marcin Kulik <m@ku1ik.com>
2014-08-08 10:42:24 +0200

    Log all requests

--- git-http-multi-backend.go
+++ git-http-multi-backend.go
@@ -8,6 +8,29 @@ import (
 	"net/http/cgi"
 )
 
+type Handler struct {
+	cgiHandler *cgi.Handler
+}
+
+func NewHandler(reposDir, backendCommand string) *Handler {
+	return &Handler{
+		&cgi.Handler{
+			Path: "/bin/sh",
+			Args: []string{"-c", backendCommand},
+			Dir:  ".",
+			Env: []string{
+				fmt.Sprintf("GIT_PROJECT_ROOT=%v", reposDir),
+				"GIT_HTTP_EXPORT_ALL=1",
+			},
+		},
+	}
+}
+
+func (h *Handler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	log.Println(req)
+	h.cgiHandler.ServeHTTP(rw, req)
+}
+
 func main() {
 	var (
 		reposDir       = flag.String("r", ".", "Directory containing git repositories")
@@ -16,16 +39,6 @@ func main() {
 	)
 	flag.Parse()
 
-	handler := &cgi.Handler{
-		Path: "/bin/sh",
-		Args: []string{"-c", *backendCommand},
-		Dir:  ".",
-		Env: []string{
-			fmt.Sprintf("GIT_PROJECT_ROOT=%v", *reposDir),
-			"GIT_HTTP_EXPORT_ALL=1",
-		},
-	}
-
-	http.Handle("/", handler)
+	http.Handle("/", NewHandler(*reposDir, *backendCommand))
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
